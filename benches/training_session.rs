94658057c4e16080cef2e99600428a8e43b2e5a6
Peter Higley <phigley@gmail.com>
2018-01-18 11:52:05 -0800

    Replaced test::bench with criterion.

--- benches/training_session.rs
+++ benches/training_session.rs
@@ -1,10 +1,12 @@
-#![feature(test)]
-
 extern crate rand;
 extern crate taxi;
-extern crate test;
+
+#[macro_use]
+extern crate criterion;
 
 use rand::Isaac64Rng;
+
+use criterion::Criterion;
 
 use taxi::world::World;
 use taxi::state::State;
@@ -12,6 +14,9 @@ use taxi::runner::{run_training_session, Probe};
 use taxi::qlearner::QLearner;
 use taxi::rmax::RMax;
 use taxi::factoredrmax::FactoredRMax;
+
+criterion_group!(trainers, qlearner, rmax, factored_rmax);
+criterion_main!(trainers);
 
 struct SessionData {
     world: World,
@@ -70,48 +75,51 @@ impl Default for SessionData {
     }
 }
 
-#[bench]
-fn qlearner(b: &mut test::Bencher) {
+fn qlearner(c: &mut Criterion) {
     let data = SessionData::default();
     let source_rng = Isaac64Rng::new_unseeded();
 
-    b.iter(|| {
-        let mut qlearner = QLearner::new(&data.world, 0.1, 0.3, 0.6, false);
-        let mut rng = source_rng;
+    c.bench_function("qmax", |b| {
+        b.iter(|| {
+            let mut qlearner = QLearner::new(&data.world, 0.1, 0.3, 0.6, false);
+            let mut rng = source_rng;
 
-        run_training_session(&data.world, &data.probes, 1, 100, &mut qlearner, &mut rng)
-    })
+            run_training_session(&data.world, &data.probes, 1, 100, &mut qlearner, &mut rng)
+        })
+    });
 }
 
-#[bench]
-fn rmax(b: &mut test::Bencher) {
+fn rmax(c: &mut Criterion) {
     let data = SessionData::default();
     let source_rng = Isaac64Rng::new_unseeded();
 
-    b.iter(|| {
-        let mut rmax = RMax::new(&data.world, 0.3, 1.0, 1.0e-6);
-        let mut rng = source_rng;
+    c.bench_function("rmax", |b| {
+        b.iter(|| {
+            let mut rmax = RMax::new(&data.world, 0.3, 1.0, 1.0e-6);
+            let mut rng = source_rng;
 
-        run_training_session(&data.world, &data.probes, 1, 10, &mut rmax, &mut rng)
-    })
+            run_training_session(&data.world, &data.probes, 1, 10, &mut rmax, &mut rng)
+        })
+    });
 }
 
-#[bench]
-fn factored_rmax(b: &mut test::Bencher) {
+fn factored_rmax(c: &mut Criterion) {
     let data = SessionData::default();
     let source_rng = Isaac64Rng::new_unseeded();
 
-    b.iter(|| {
-        let mut factored_rmax = FactoredRMax::new(&data.world, 0.3, 1.0, 1.0e-6);
-        let mut rng = source_rng;
+    c.bench_function("factored_rmax", |b| {
+        b.iter(|| {
+            let mut factored_rmax = FactoredRMax::new(&data.world, 0.3, 1.0, 1.0e-6);
+            let mut rng = source_rng;
 
-        run_training_session(
-            &data.world,
-            &data.probes,
-            1,
-            10,
-            &mut factored_rmax,
-            &mut rng,
-        )
-    })
+            run_training_session(
+                &data.world,
+                &data.probes,
+                1,
+                10,
+                &mut factored_rmax,
+                &mut rng,
+            )
+        })
+    });
 }
